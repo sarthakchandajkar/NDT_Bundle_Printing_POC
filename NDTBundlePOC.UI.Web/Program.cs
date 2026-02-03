72c3c36f9531617d5304d682af3abb682d043bff
sarthakchandajkar <sarthak.chandajkar@gmail.com>
2026-02-03 11:27:29 +0400

    Fix: build error (Task) and nullable warnings
    - Add using System.Threading.Tasks in Web Program.cs (fixes CS0103 Task not found)
    - Initialize PipeCountingActivity.PipeType and Source to string.Empty (fixes CS8618 warnings)

--- NDTBundlePOC.UI.Web/Program.cs
+++ NDTBundlePOC.UI.Web/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
